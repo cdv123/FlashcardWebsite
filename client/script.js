9660e8c1d8d64e30b14169c7b0e7747468a3ed89
cdv123 <charles.duboisveltman@gmail.com>
2023-02-22 12:11:21 +0000

    added more instances to json files

--- client/script.js
+++ client/script.js
@@ -1,41 +1,40 @@
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    let rightArrow = document.getElementById("right-arrow")
-    let leftArrow = document.getElementById("left-arrow")
-    let container = document.querySelector(".container")
-    let flashcards = document.querySelectorAll(".flashcards")
-    let cols = document.querySelectorAll(".col")
-    let stars = document.querySelectorAll(".star")
-    for (let i = 0; i<stars.length; i++){
-        stars[i].addEventListener("click",function(){
-            for (let j = 0; j<= i; j++){
-                stars[j].classList.add("yellow")
-            }
-            for (let j = i+1; j<stars.length; j++){
-                stars[j].classList.remove("yellow")
-            }
-        })
-    }
-    for (let i = 0; i<flashcards.length; i++){
-        flashcards[i].addEventListener("click",function (){
-            cols[i].classList.add("fade")
-            setTimeout(function flip(){
-                cols[i].classList.add("fade-in")
-                cols[i].classList.remove("fade")
-            },200)
-        })
-    }
-    rightArrow.addEventListener("click", function (){
-        container.classList.add("fade");
-        setTimeout(fade,200)
+  const rightArrow = document.getElementById('right-arrow')
+  const leftArrow = document.getElementById('left-arrow')
+  const container = document.querySelector('.container')
+  const flashcards = document.querySelectorAll('.flashcards')
+  const cols = document.querySelectorAll('.col')
+  const stars = document.querySelectorAll('.star')
+  for (let i = 0; i < stars.length; i++) {
+    stars[i].addEventListener('click', function () {
+      for (let j = 0; j <= i; j++) {
+        stars[j].classList.add('yellow')
+      }
+      for (let j = i + 1; j < stars.length; j++) {
+        stars[j].classList.remove('yellow')
+      }
     })
-    leftArrow.addEventListener("click", function (){
-        container.classList.add("fade");
-        setTimeout(fade,200)
+  }
+  for (let i = 0; i < flashcards.length; i++) {
+    flashcards[i].addEventListener('click', function () {
+      cols[i].classList.add('fade')
+      setTimeout(function flip () {
+        cols[i].classList.add('fade-in')
+        cols[i].classList.remove('fade')
+      }, 200)
     })
-    function fade(){
-        container.classList.add("fade-in")
-        container.classList.remove("fade")
-    }
-});
-
+  }
+  rightArrow.addEventListener('click', function () {
+    container.classList.add('fade')
+    setTimeout(fade, 200)
+  })
+  leftArrow.addEventListener('click', function () {
+    container.classList.add('fade')
+    setTimeout(fade, 200)
+  })
+  function fade () {
+    container.classList.add('fade-in')
+    container.classList.remove('fade')
+  }
+})
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const app = require('./app.js');
-const hostname = '127.0.0.1';
-const port = 8080;
+const app = require('./app.js')
+const hostname = '127.0.0.1'
+const port = 8080
 
 app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/ -- yippee!`);
-  });
-
+  console.log(`Server running at http://${hostname}:${port}/ -- yippee!`)
+})
